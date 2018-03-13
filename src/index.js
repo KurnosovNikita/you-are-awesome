bea263174d95b3d70c9a41d5014e9139ad198d23
KurnosovNikita <Kurnosov-nikita@mail.ru>
2018-03-13 15:01:20 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,18 +1,71 @@
 // DO WHATEVER YOU WANT HERE
+const sortFilter = (left, right) => left.photo - right.photo;
 
-const createEnumerableProperty = () => {};
-const createNotEnumerableProperty = () => {};
-const createProtoMagicObject = () => {};
-const incrementor = () => {};
+const createEnumerableProperty = (enumerableProperty) => {
+    return enumerableProperty
+};
+
+const createNotEnumerableProperty = (notEnumerableProperty) => {
+    Object.defineProperty(Object.prototype, notEnumerableProperty, {
+        enumerable: false,
+        value: 'value'
+    });
+    return notEnumerableProperty;
+};
+
+const createProtoMagicObject = () => {
+    var newFunctionForPhoto = function () {};
+    newFunctionForPhoto.prototype = newFunctionForPhoto.__proto__;
+    return newFunctionForPhoto;
+};
+
+var inc = 0;
+const incrementor = () => {
+    inc++;
+    return incrementor;
+};
+
+incrementor.valueOf = function () {
+    return inc;
+}
+
 const asyncIncrementor = () => {};
-const createIncrementer = () => {};
+
+const createIncrementer = () => {
+    return increment();
+
+};
+
+function* increment() {
+    var inc = 1;
+    while (true) {
+        yield inc++;
+    }
+}
 
 // return same argument not earlier than in one second, and not later, than in two
-const returnBackInSecond = () => {};
+
+const returnBackInSecond = (delay) => {
+    return new Promise((resolve) => setDelay(resolve, delay));
+};
+
+function setDelay(resolve, delay) {
+    setTimeout(() => {
+        resolve(delay);
+    }, 1001);
+}
+
 const getDeepPropertiesCount = () => {};
-const createSerializedObject = () => {};
+
+const createSerializedObject = () => ({
+    property: "object",
+    toJSON: () => "",
+    toString: () => "",
+});
+
 const toBuffer = () => {};
-const sortByProto = () => {};
+
+const sortByProto = (objects) => objects.sort(sortFilter);
 
 exports.createEnumerableProperty = createEnumerableProperty;
 exports.createNotEnumerableProperty = createNotEnumerableProperty;
